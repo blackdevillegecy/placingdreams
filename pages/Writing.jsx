2ad8ccdda603804864d3c4b6039173b6042b418d
Ayush Gautam <gautamayush953@gmail.com>
2022-11-10 03:32:55 +0530

    pd updated

--- pages/Writing.jsx
+++ pages/Writing.jsx
@@ -4,7 +4,7 @@ function Writing() {
   return (<div className='body2'>
     <div className='overall'>
     <h1 className='main' style={{background: 'linear-gradient(90deg, #EE82EE 0%, #00D1FF 100%)', backgroundClip: 'text', WebkitBackgroundClip: 'text', WebkitTextFillColor: 'transparent'}}>Writing<br/><br/></h1>
-    <p>A writer creates material for visual media. For television, advertisements, video games, feature films, short films, and other media, they compose screenplays. A script's dialogue, characters, and plot are created by them.</p>
+    <p>A writer creates material for visual media. For television, advertisements, video games, feature films, short films, and other media, they compose screenplays. A script`&apos;`s dialogue, characters, and plot are created by them.</p>
     <h4>Career in Writing</h4>
     <p>Screenwriting is one of the most lucrative career in the recent times and hence the film city is seeing a rapid growth in the number of people trying to get a grasp in this field. Screenwriting is an art which requires imagination, every decision made by them can completely
       change how the audience feel the emotions and how they interpret it. Students at Placing Dreams get to make short films international festivals and from students get to learn a lot and experiment with their skills, which also helps themi dentify what they are good at. </p>
