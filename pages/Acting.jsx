d978120438dbca7dc907796e6b140e78a64b9e5a
atronos <slenderblendertender@gmail.com>
2022-12-15 05:35:37 +0530

    aj4

--- pages/Acting.jsx
+++ pages/Acting.jsx
@@ -22,10 +22,16 @@ function Acting() {
       <h4>Career in Acting</h4>
       <p>Most of the people who come to the city of Mumbai initially start with acting. Acting is a highly competative field and hence, the stakes are high, but, one should not get disheartned by this as with the exponential growth of the film industry 
       there are new opportunities bubbling up everywhere. There auditions going everywhere in the city, so, the real job for an actor is to be best version of themselves and there success will be inevitable. To perfect their art, an actor need to put a lot
-      of sweat and hours, thats where the difference can be observed between the people who `&apos;`WANT`&apos;` it and the ones who `&quot;`GET`&quot;` it. 
-
+      of sweat and hours, thats where the difference can be observed between the people who `&apos;`WANT`&apos;` it and the ones who `&quot;`GET`&quot;` it. Acting can be a challenging and competitive field, as there are often many talented actors vying for
+      the same roles. However, for those who are dedicated and hardworking, acting can also be a fulfilling and rewarding career. Actors have the opportunity to bring joy and entertainment to audiences, and to explore and express the full range of human emotions through their art.
+      Acting is a form of storytelling that allows actors to bring characters and their stories to life. It requires a high level of emotional intelligence and the ability to empathize with a wide range of characters and personalities. Actors need to be able to interpret scripts and
+       understand their characters' motivations, goals, and obstacles, and then bring those characters to life through their performances.
+      To become an actor, one typically needs to have a passion for storytelling and a willingness to take risks and try new things. Acting also requires a certain level of physical and emotional vulnerability, as actors must be open and honest in their performances in order to connect 
+      with their characters and audiences.
+      Training and education are important for actors, as they need to learn various techniques and skills in order to excel in their craft. 
       Students of Placing Dreams get the best possible training from the best people in the field. Nearly every student from the institute get to start with a project from recognsed media houses. At Placing Dreams Students are taught to treat work like worship, 
-      The values of disicipline and willingness to make scarifice is ingrained in every student from the beginning. The opportunities are plenty, all one need is dedication and hard work. The syllabus followed at the institute is handcrafted by the experts in the industry and tries to shed light on all the dark corners which might be left in the courses by other institutions.
+      The values of disicipline and willingness to make scarifice is ingrained in every student from the beginning. The opportunities are plenty, all one need is dedication and hard work. The syllabus followed at the institute is handcrafted by the experts in the 
+      industry and tries to shed light on all the dark corners which might be left in the courses by other institutions.
       </p>
       <h1 className='main' style={{background: 'linear-gradient(90deg, #EE82EE 0%, #00D1FF 100%)', backgroundClip: 'text', WebkitBackgroundClip: 'text', WebkitTextFillColor: 'transparent'}}>Acting Syllabus<br/><br/></h1>
       <Image src="/features/acting.jpeg" alt ='' width="700" height="350" />
--- pages/FilmMaking.jsx
+++ pages/FilmMaking.jsx
@@ -20,7 +20,11 @@ function FilmMaking() {
       <h4> A Peek into the Film Making Course</h4><br/>
       <p>Film Making is a perfect soup which has the flavor of all other courses in the right amount. Students are taught about acting and all the technicalities related to the art, Then the course also focuses on the way of direction and the study of the job in detail
         The course will also take a dive in the fields of writing and editing, hence, covering all the ingredients required to create the next masterpiece for the world to see. By taking this course a student can expect to be well versed in the ins and outs of the industry and along with the knowledge
-        they will also be gaining practical knowledge through different on-site internships focusing on the actual workflow and the way tasks are handeled on the ground level.
+        they will also be gaining practical knowledge through different on-site internships focusing on the actual workflow and the way tasks are handeled on the ground level. 
+        Filmmaking is a collaborative art form that brings together a team of creative and technical professionals to create a finished film. This team typically includes a writer, who creates the initial concept and screenplay; a director, who brings the screenplay to life by guiding the actors and crew; a cinematographer, who creates the visual look of the film; and an editor, who assembles the footage into a cohesive story.
+        Filmmaking also involves many other disciplines, such as set design, costume design, makeup, sound design, and special effects. These professionals work together to bring the filmmaker's vision to life and create a finished film that is engaging and compelling.
+        To become a filmmaker, one typically needs to have a passion for storytelling and the ability to bring ideas to life on screen. Filmmaking also requires a certain level of technical expertise, as filmmakers need to understand how to use cameras, lighting, and other equipment to create their desired visual effects.
+        Training and education are important for filmmakers, as they need to learn about the various disciplines involved in making films, as well as the technical skills required to use the equipment and software.
       </p><br/>
        <Image src="/features/direction.jpeg" alt="" width="700" height="350" />
                                    
